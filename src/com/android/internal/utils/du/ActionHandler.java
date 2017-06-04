ezio84/abc-n_external_DUtils

Smartbar media arrows: more code improvements [2/2]

src/com/android/internal/utils/du/ActionHandler.java
@@ -170,7 +170,9 @@ public class ActionHandler {
         EditingSmartbar(SYSTEMUI_TASK_EDITING_SMARTBAR, SYSTEMUI, "label_action_editing_smartbar", "ic_sysbar_editing_smartbar"),
         SplitScreen(SYSTEMUI_TASK_SPLIT_SCREEN, SYSTEMUI, "label_action_split_screen", "ic_sysbar_docked"),
         OneHandedModeLeft(SYSTEMUI_TASK_ONE_HANDED_MODE_LEFT, SYSTEMUI, "label_action_one_handed_mode_left", "ic_sysbar_one_handed_mode_left"),
-        OneHandedModeRight(SYSTEMUI_TASK_ONE_HANDED_MODE_RIGHT, SYSTEMUI, "label_action_one_handed_mode_right", "ic_sysbar_one_handed_mode_right");
+        OneHandedModeRight(SYSTEMUI_TASK_ONE_HANDED_MODE_RIGHT, SYSTEMUI, "label_action_one_handed_mode_right", "ic_sysbar_one_handed_mode_right"),
+        MediaArrowLeft(SYSTEMUI_TASK_MEDIA_PREVIOUS, SYSTEMUI, "label_action_media_left", "ic_skip_previous"),
+        MediaArrowRight(SYSTEMUI_TASK_MEDIA_NEXT, SYSTEMUI, "label_action_media_right", "ic_skip_next");
 
         String mAction;
         String mResPackage;
@@ -209,7 +211,8 @@ public class ActionHandler {
             SystemAction.EditingSmartbar, SystemAction.SplitScreen,
             SystemAction.Home, SystemAction.Ime,
             SystemAction.RegionScreenshot, SystemAction.OneHandedModeLeft,
-            SystemAction.OneHandedModeRight
+            SystemAction.OneHandedModeRight, SystemAction.MediaArrowLeft,
+            SystemAction.MediaArrowRight
     };
 
     public static class ActionIconResources {
@@ -253,7 +256,9 @@ public class ActionHandler {
                     || TextUtils.equals(action, SYSTEMUI_TASK_IME_NAVIGATION_LEFT)
                     || TextUtils.equals(action, SYSTEMUI_TASK_IME_NAVIGATION_RIGHT)
                     || TextUtils.equals(action, SYSTEMUI_TASK_IME_NAVIGATION_UP)
-                    || TextUtils.equals(action, SYSTEMUI_TASK_IME_SWITCHER)) {
+                    || TextUtils.equals(action, SYSTEMUI_TASK_IME_SWITCHER)
+                    || TextUtils.equals(action, SYSTEMUI_TASK_MEDIA_PREVIOUS)
+                    || TextUtils.equals(action, SYSTEMUI_TASK_MEDIA_NEXT)) {
                 continue;
             } else if (TextUtils.equals(action, SYSTEMUI_TASK_WIFIAP)
                     && !DUActionUtils.deviceSupportsMobileData(context)) {

Team-OctOS/platform_external_DUtils

Change-Id: I9de71f0fd25ec2a7f1bee99ea36125e1a832cf81

src/com/android/internal/utils/du/ActionHandler.java
@@ -172,7 +172,9 @@ public class ActionHandler {
         ClearNotifications(SYSTEMUI_TASK_CLEAR_NOTIFICATIONS, SYSTEMUI, "label_action_clear_notifications", "ic_sysbar_clear_notifications"),
         VolumePanel(SYSTEMUI_TASK_VOLUME_PANEL, SYSTEMUI, "label_action_volume_panel", "ic_sysbar_volume_panel"),
         EditingSmartbar(SYSTEMUI_TASK_EDITING_SMARTBAR, SYSTEMUI, "label_action_editing_smartbar", "ic_sysbar_editing_smartbar"),
-        SplitScreen(SYSTEMUI_TASK_SPLIT_SCREEN, SYSTEMUI, "label_action_split_screen", "ic_sysbar_docked");
+        SplitScreen(SYSTEMUI_TASK_SPLIT_SCREEN, SYSTEMUI, "label_action_split_screen", "ic_sysbar_docked"),
+        MediaArrowLeft(SYSTEMUI_TASK_MEDIA_PREVIOUS, SYSTEMUI, "label_action_media_left", "ic_skip_previous"),
+        MediaArrowRight(SYSTEMUI_TASK_MEDIA_NEXT, SYSTEMUI, "label_action_media_right", "ic_skip_next");
 
         String mAction;
         String mResPackage;
@@ -211,7 +213,8 @@ public class ActionHandler {
             SystemAction.ImeArrowUp, SystemAction.InAppSearch,
             SystemAction.VolumePanel, SystemAction.ClearNotifications,
             SystemAction.EditingSmartbar, SystemAction.SplitScreen,
-            SystemAction.RegionScreenshot
+            SystemAction.RegionScreenshot, SystemAction.MediaArrowLeft, 
+            SystemAction.MediaArrowRight
     };
 
     public static class ActionIconResources {
@@ -255,7 +258,9 @@ public class ActionHandler {
                     || TextUtils.equals(action, SYSTEMUI_TASK_IME_NAVIGATION_LEFT)
                     || TextUtils.equals(action, SYSTEMUI_TASK_IME_NAVIGATION_RIGHT)
                     || TextUtils.equals(action, SYSTEMUI_TASK_IME_NAVIGATION_UP)
-                    || TextUtils.equals(action, SYSTEMUI_TASK_IME_SWITCHER)) {
+                    || TextUtils.equals(action, SYSTEMUI_TASK_IME_SWITCHER)
+                    || TextUtils.equals(action, SYSTEMUI_TASK_MEDIA_PREVIOUS)
+                    || TextUtils.equals(action, SYSTEMUI_TASK_MEDIA_NEXT)) {
                 continue;
             } else if (TextUtils.equals(action, SYSTEMUI_TASK_WIFIAP)
                     && !DUActionUtils.deviceSupportsMobileData(context)) {